39aa09b5065d09d720111f8b966e9880ccf452e6
JLopezBedoya <127346257+JLopezBedoya@users.noreply.github.com>
2023-04-24 22:25:04 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function App(){
       <h2>Scrum master: Jorge Lopez</h2>
       <h2>Desarrolladores:</h2>
       <h2>-Miguel Perez</h2>
-      <h2>-Dylan cardenas</h2>
+      <h2>-Dylan Cardenas</h2>
       <h2>-Carlos Quintero</h2>
       <h2>-kenner rocha</h2>
     </div>
